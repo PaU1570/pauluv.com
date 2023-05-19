a46ff46bb3a5c0ae9543816eade597563a0c6413
Paul Uriarte Vicandi <pauluv@ni.eus>
2023-05-20 00:10:05 +0200

    Added images to blog posts.

--- nirepage/blog.py
+++ nirepage/blog.py
@@ -1,13 +1,20 @@
 import functools
-import markdown
+import frontmatter
+import markdown2
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, current_app
 )
 
 from markupsafe import escape
 
 bp = Blueprint('blog', __name__, url_prefix='/blog')
+
+# Route for the custom "content" folder
+@bp.route('/content/<path:filename>')
+def post_image(filename):
+    return send_from_directory('content', filename)
+
 
 @bp.route('/')
 def index():
@@ -17,7 +24,13 @@ def index():
 @bp.route('/<post_category>/<post_title>')
 def show_post(post_category, post_title):
     # Read the .md file content
-    with open('example.md', 'r') as file:
-        md_content = file.read()
+    abs_path = current_app.root_path
+    with open('{0}/content/{1}/{2}/{2}.md'.format(abs_path, escape(post_category), escape(post_title)), 'r') as file:
+        post = frontmatter.load(file)
 
-    return render_template('blog/post.html', category = post_category, title = post_title)
+    # replace relative image paths with correct paths
+    fig_path = '/content/{}/{}/'.format(escape(post_category), escape(post_title))
+    post_content = post.content.replace('<img src="', f'<img class="img-fluid mx-auto d-block" src="{fig_path}')
+
+    post_content_html = markdown2.markdown(post_content)
+    return render_template('blog/post.html', post_metadata = post.metadata, post_content_html = post_content_html)
---
+++ nirepage/content/test/testpost/testpost.md
@@ -0,0 +1,20 @@
+---
+title: "My first post" 
+date: 2023-05-19T23:05:38 
+author: "Paul U.V."
+categories:
+  - Programming
+tags: 
+  - html
+  - website
+  - flask
+---
+
+### Hello, blog!
+
+This is the first post on my personal website built with [flask](https://flask.palletsprojects.com/en/2.3.x/). I can also
+insert images like this:
+
+<img src="Tree.JPG"></img>
+
+This is fun.
